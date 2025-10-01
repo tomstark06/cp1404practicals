29b5df6ce23f9b7a4160a46910e07607ad9aad5c
Thomas Stark <thomas.stark@my.jcu.edu.au>
2025-10-02 08:22:44 +1000

    Add and complete randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,29 @@
+"""Randoms Practical File"""
+
+import random
+
+# What did you see on line 1?
+"8"
+# Smallest?
+"5"
+# Largest?
+"20"
+
+# What did you see on line 2?
+"7"
+# Smallest?
+"3"
+# Largest?
+"9"
+# Could line 2 have produced a 4?
+"No"
+
+# What did you see on line 3?
+"3.3314325386343953"
+# Smallest
+"2.5"
+# Largest
+"5.5"
+
+# Random number between 1 and 100:
+print(random.uniform(1, 100))
