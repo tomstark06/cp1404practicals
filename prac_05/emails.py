6f11b786018831772957d5af0cb89201aa37730a
Thomas Stark <thomas.stark@my.jcu.edu.au>
2025-10-16 14:18:00 +1000

    Complete emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,21 @@
 """
 Emails exercise
 Estimate: 20 minutes
-Actual: 2:00 -
+Actual: 17 minutes
 """
+
+email_to_name = {}
+email = input("Email: ")
+while email != "":
+    names = email[:email.index("@")].split(".")
+    full_name = " ".join(name.capitalize() for name in names)
+    is_correct_name = input(f"Is your name {full_name}? (Y/n) ").lower()
+    if is_correct_name == "y" or is_correct_name == "":
+        email_to_name[email] = full_name
+    else:
+        new_name = input("Name: ")
+        email_to_name[email] = new_name
+    email = input("Email: ")
+print()
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
