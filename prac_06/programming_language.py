4730695e7d1f7052cb1fefb1d8eec504ca011557
Thomas Stark <thomas.stark@my.jcu.edu.au>
2025-10-30 12:14:23 +1000

    Create ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""Programming language exercise - ProgrammingLanguage class definition."""
+
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, typing="", is_reflective=False, year=0):
+        """Initialise a ProgrammingLanguage instance."""
+        self.typing = typing
+        self.is_reflective = is_reflective
+        self.year = year
+
+    def is_dynamic(self):
+        """Return a boolean if the ProgrammingLanguage is dynamic or not."""
+        return self.typing == "Dynamic"
