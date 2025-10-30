f4d090b36f14e767ffa1dbac7170f3e094aea671
Thomas Stark <thomas.stark@my.jcu.edu.au>
2025-10-30 12:02:26 +1000

    Complete car and used_car files by adding name as an argument and adding the __str__ method

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Ferrari", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
+
+    limo = Car("Kia Rio", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
 
 
 main()
