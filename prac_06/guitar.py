9a1157f2a6dabf09e451e91ed485e8da3c880183
Thomas Stark <thomas.stark@my.jcu.edu.au>
2025-10-30 12:35:34 +1000

    Complete Guitar class definition

---
+++ prac_06/guitar.py
@@ -0,0 +1,22 @@
+"""Guitar class definition"""
+
+CURRENT_YEAR = 2025
+VINTAGE_THRESHOLD = 50
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return Guitar.get_age(self) >= 50
